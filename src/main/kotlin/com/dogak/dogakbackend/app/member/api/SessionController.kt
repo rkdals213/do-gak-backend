03d01e26cd62f5eed45a16e46cebc15dcb9c93ca
rkdals213 <rkdals213@naver.com>
2022-01-31 17:54:08 +0900

    adsf

--- src/main/kotlin/com/dogak/dogakbackend/app/member/api/SessionController.kt
+++ src/main/kotlin/com/dogak/dogakbackend/app/member/api/SessionController.kt
@@ -2,12 +2,8 @@ package com.dogak.dogakbackend.app.member.api
 
 import com.dogak.dogakbackend.app.member.application.MemberService
 import com.dogak.dogakbackend.app.member.dto.KakaoLoginPayload
-import com.dogak.dogakbackend.common.config.WebConfig.Companion.EXPIRATION
-import com.dogak.dogakbackend.common.config.WebConfig.Companion.JWT_COOKIE_NAME
-import com.dogak.dogakbackend.common.security.CookieConfig
 import com.dogak.dogakbackend.common.security.CookieManager
 import com.dogak.dogakbackend.common.security.DefaultJwtService
-import com.dogak.dogakbackend.common.security.HttpSupport
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import javax.servlet.http.HttpServletRequest
